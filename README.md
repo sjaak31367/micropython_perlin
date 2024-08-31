1fe5653d49fa7fbe99b8590b199a26a2b0b6a6bb
Sjaak van Montfort <sjaak31367@gmail.com>
2024-08-31 17:12:18 +0200

    v2. undefined symbol: __extendsfdf2 (or fmodf). Switching to compiling with the firmware so it definitely has access to the same libraries.

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 #### A module for micropython which adds the ability to generate Perlin noise  
 
 Developed against micropython [v1.24.0-preview.218.gb704ff66c.dirty](https://github.com/micropython/micropython/commit/b704ff66c3e034c36e548eb0b9874871b5f3b5d0) with a WEMOS S2 Mini (esp32s2) and ESP-IDF [v5.2.2](https://github.com/espressif/esp-idf/releases/tag/v5.2.2) with Python [3.12.5](https://www.python.org/downloads/release/python-3125/).  
+
+----
+#### Credits  
+Based off Flafla2's [Perlin.cs](https://gist.github.com/Flafla2/1a0b9ebef678bbce3215)  
--- natmod/_perlin.c
+++ natmod/_perlin.c
@@ -1,46 +1,92 @@
 // Include the header file to get access to the MicroPython API
 #include "py/dynruntime.h"
 #include <math.h>
+#include <stdio.h>
+#include "_noise.h"
 
 #define lerp(t, a, b) ((a) + (t) * ((b) - (a)))
 
-// Helper function to compute factorial
-static mp_int_t factorial_helper(mp_int_t x) {
-    if (x == 0) {
-        return 1;
+static inline float grad2(const int hash, const float x, const float y) {
+    const int h = hash & 15;
+    return x * GRAD3[h][0] + y * GRAD3[h][1];
+}
+
+float noise2(float x, float y, const float repeatx, const float repeaty, const int base) {
+    float fx, fy;
+    int A, AA, AB, B, BA, BB;
+    int i = (int)floorf(fmodf(x, repeatx));
+    int j = (int)floorf(fmodf(y, repeaty));
+    int ii = (int)fmodf(i + 1, repeatx);
+    int jj = (int)fmodf(j + 1, repeaty);
+    i = (i & 255) + base;
+    j = (j & 255) + base;
+    ii = (ii & 255) + base;
+    jj = (jj & 255) + base;
+
+    x -= floorf(x); y -= floorf(y);
+    fx = x*x*x * (x * (x * 6 - 15) + 10);
+    fy = y*y*y * (y * (y * 6 - 15) + 10);
+
+    A  = PERM[i];
+    AA = PERM[A + j];
+    AB = PERM[A + jj];
+    B  = PERM[ii];
+    BA = PERM[B + j];
+    BB = PERM[B + jj];
+        
+    return lerp(fy, lerp(fx, grad2(PERM[AA], x, y),
+                             grad2(PERM[BA], x - 1, y)),
+                    lerp(fx, grad2(PERM[AB], x, y - 1),
+                             grad2(PERM[BB], x - 1, y - 1)));
+}
+
+static mp_obj_t py_noise2(size_t n_args, const mp_obj_t* args) {
+    //if (n_args == 3 || n_args == 4) {
+        //mp_obj_t self = args[0];
+        mp_float_t _x = mp_obj_get_float(args[1]);
+        mp_float_t _y = mp_obj_get_float(args[2]);
+        mp_int_t _octaves = 1;
+        if (n_args == 4) {
+            _octaves = mp_obj_get_int(args[3]);
+        }
+    //} else {
+    //    mp_raise_ValueError(MP_ERROR_TEXT("Function requires 3 or 4 arguments!"));
+    //    return NULL;
+    //}
+    
+    float x = _x;
+    float y = _y;
+    int octaves = (mp_int_t) _octaves;
+    float persistence = 0.5f;
+    float lacunarity = 2.0f;
+    float repeatx = 1024; // arbitrary
+    float repeaty = 1024; // arbitrary
+    int base = 0;
+
+    if (octaves == 1) {
+        // Single octave, return simple noise
+        return (mp_obj_t *) mp_obj_new_float_from_d((double) noise2(x, y, repeatx, repeaty, base));
+    } else if (octaves > 1) {
+        int i;
+        float freq = 1.0f;
+        float amp = 1.0f;
+        float max = 0.0f;
+        float total = 0.0f;
+
+        for (i = 0; i < octaves; i++) {
+            total += noise2(x * freq, y * freq, repeatx * freq, repeaty * freq, base) * amp;
+            max += amp;
+            freq *= lacunarity;
+            amp *= persistence;
+        }
+        return (mp_obj_t *) mp_obj_new_float_from_d((double) (total / max));
+    } else {
+        mp_raise_ValueError(MP_ERROR_TEXT("Expected octaves value > 0"));
+        return NULL;
     }
-    return x * factorial_helper(x - 1);
-}
-
-static mp_float_t _tost(mp_float_t f) {
-    //float* c = 2.0f; //mp_obj_new_float(2.0f)
-    /*mp_obj_t a = mp_obj_new_float_from_f(2.0f);
-    mp_float_t b = mp_obj_get_float_to_f(f);
-    return mp_obj_new_float_from_f(mp_obj_get_float_to_f(a)*b);*/
-    /*mp_float_t result;
-    mp_float_t two = mp_obj_new_float(2.0f);
-    result = f / two;//mp_obj_get_float_to_f(two);
-    return result;*/
-    //mp_float_t two = MICROPY_FLOAT_CONST(2.0);
-    return f / 2;
-}
-
-// This is the function which will be called from Python, as factorial(x)
-static mp_obj_t factorial(mp_obj_t x_obj) {
-    // Extract the integer from the MicroPython input object
-    mp_int_t x = mp_obj_get_int(x_obj);
-    // Calculate the factorial
-    mp_int_t result = factorial_helper(x);
-    // Convert the result to a MicroPython integer object and return it
-    return mp_obj_new_int(result);
-}
-static mp_obj_t tost(mp_obj_t f_obj) {
-    mp_float_t input = mp_obj_get_float_to_f(f_obj);
-    return mp_obj_new_float(_tost(input));
 }
 // Define a Python reference to the function above
-static MP_DEFINE_CONST_FUN_OBJ_1(factorial_obj, factorial);
-static MP_DEFINE_CONST_FUN_OBJ_1(tost_obj, tost);
+static MP_DEFINE_CONST_FUN_OBJ_VAR(py_noise2_obj, 4, py_noise2);
 
 // This is the entry point and is called when the module is imported
 mp_obj_t mpy_init(mp_obj_fun_bc_t *self, size_t n_args, size_t n_kw, mp_obj_t *args) {
@@ -48,8 +94,7 @@ mp_obj_t mpy_init(mp_obj_fun_bc_t *self, size_t n_args, size_t n_kw, mp_obj_t *a
     MP_DYNRUNTIME_INIT_ENTRY
 
     // Make the function available in the module's namespace
-    mp_store_global(MP_QSTR_factorial, MP_OBJ_FROM_PTR(&factorial_obj));
-    mp_store_global(MP_QSTR_tost, MP_OBJ_FROM_PTR(&tost_obj));
+    mp_store_global(MP_QSTR_noise2, MP_OBJ_FROM_PTR(&py_noise2_obj));
 
     // This must be last, it restores the globals dict
     MP_DYNRUNTIME_INIT_EXIT
